aefdbaf428708a1142f468a3d9065c7595e8aa05
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-01-27 11:19:41 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+const App = () => {
+  useEffect(() => {
+    // Тут будемо виконувати HTTP-запит
+  }, []);
+
+  return (
+    <div>
+      <h1>Latest articles</h1>
+    </div>
+  );
+};
